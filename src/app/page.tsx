51d01351deb698fab729d7df59ee1304984f0daa
oluwaseunladeinde <oluwaseun.ladeinde@yahoo.com>
2024-09-13 14:41:32 +0100

    feat: implement user button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { useAuthActions } from "@convex-dev/auth/react";
+
+import { UserButton } from "@/features/auth/components/user-button";
+
 
 export default function Home() {
 
   const { signOut } = useAuthActions();
   return (
     <div>
-      <Button onClick={() => signOut()}>
-        SignOut
-      </Button>
+      <UserButton />
     </div>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export const getInitialCharacters = (fullName: string | undefined): string => {
+
+  if (fullName === undefined) {
+    return '';
+  }
+  // Split the full name into an array of words
+  const words = fullName.split(' ');
+
+  // Extract the first character from the first and last words
+  const firstNameInitial = words[0][0];
+  const lastNameInitial = words[words.length - 1][0];
+
+  // Combine the initials
+  return `${firstNameInitial}${lastNameInitial}`;
+}
