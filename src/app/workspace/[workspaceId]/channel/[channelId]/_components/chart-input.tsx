e29d0bb2af1422911bb30a3f9837e2117002f1ba
oluwaseunladeinde <oluwaseun.ladeinde@yahoo.com>
2024-09-23 20:37:57 +0100

    feat: upload image

--- src/app/workspace/[workspaceId]/channel/[channelId]/_components/chart-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/_components/chart-input.tsx
@@ -3,7 +3,10 @@ import { toast } from "sonner";
 import Quill from "quill";
 import { useRef, useState } from "react";
 
+import { Id } from "../../../../../../../convex/_generated/dataModel";
 import { useCreateMessage } from "@/features/messages/api/use-create-message";
+import { useGenerateUploadUrl } from "@/features/upload/api/use-generate-upload-url";
+
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useChannelId } from "@/hooks/use-channel-id";
 
@@ -11,37 +14,69 @@ const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
 interface ChartInputProps {
     placeholder: string;
-}
+};
+
+type CretaeMessageValues = {
+    channelId: Id<"channels">;
+    workspaceId: Id<"workspaces">;
+    body: string;
+    image: Id<"_storage"> | undefined;
+};
 
 export const ChartInput = ({ placeholder }: ChartInputProps) => {
 
     const [editorKey, setEditorKey] = useState(0);
     const [isPending, setIsPending] = useState(false);
 
-
-
     const editorRef = useRef<Quill | null>(null);
     const workspaceId = useWorkspaceId();
     const channelId = useChannelId();
 
     const { mutate: createMessage } = useCreateMessage();
+    const { mutate: generateUploadUrl } = useGenerateUploadUrl();
 
     const handleSubmit = async ({ body, image }: { body: string, image: File | null }) => {
         console.log({ body, image });
 
         try {
             setIsPending(true);
-            await createMessage({
-                workspaceId,
+            editorRef?.current?.enable(false);
+
+            const values: CretaeMessageValues = {
                 channelId,
+                workspaceId,
                 body,
-            }, { throwError: true });
+                image: undefined
+            }
+
+            if (image) {
+                const url = await generateUploadUrl({}, { throwError: true });
+                if (!url) {
+                    throw new Error("Url not found!");
+                }
+                const result = await fetch(url, {
+                    method: "POST",
+                    headers: { "Content-Type": image.type },
+                    body: image
+                });
+
+                if (!result.ok) {
+                    throw new Error("Failed to upload image.");
+                }
+
+                const { storageId } = await result.json();
+
+                values.image = storageId;
+            }
+
+            await createMessage(values, { throwError: true });
 
             setEditorKey((prevKey) => prevKey + 1);
         } catch (error) {
             toast.error("Failed to send message");
         } finally {
             setIsPending(false);
+            editorRef?.current?.enable(true);
         }
     };
 
